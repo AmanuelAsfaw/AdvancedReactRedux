2923509e29d34c2e866ac604beac305478ef3e5b
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-03-11 17:19:24 +0300

    initial testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import ReactDOM from 'react-dom';
+import React from 'react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+
+// });
+
+it('render without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App/>, div);
+  expect(div.innerHTML).toContain('Hi There!')
+  ReactDOM.unmountComponentAtNode(div);
 });
