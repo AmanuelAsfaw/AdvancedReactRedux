abd4b6212323d8e41a378c4a8e8f06b4ce738ed2
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-03-12 15:34:36 +0300

    React Component Test Using Enzyme Library

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,14 +1,32 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
+
 import App from "../App";
+import CommentBox from "../CommentBox";
+import CommentList from "../CommentList";
 
 it('shows a comment box', ()=> {
-    const div = document.createElement('div');
+    
+    
+    // using Enzyme library to test react components
+    // documentaion link https://enzymejs.github.io/enzyme/
+    /* 
+        Enzyme API
+            Static
+                render the given component and return plain HTML
+            Shallow
+                render *just* the given component and none of its children
+            Full DOM
+                render the component and al of its children + let us modify it afterwards
+     */
 
-    ReactDOM.render(<App/>, div);
+    const wrapped = shallow(<App />);
+    expect(wrapped.find(CommentBox).length).toEqual(1 );
+});
 
-    expect(div.innerHTML).toContain('Comment Box');
-
-    ReactDOM.unmountComponentAtNode(div);
+it('shows a comment list', ()=>{
+    const wrapped = shallow(<App/>);
+    expect(wrapped.find(CommentList).length).toEqual(1 );
 });
