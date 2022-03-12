9c7df8e362a1d5a7fa0952eb7eae4fda78475d5b
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-03-12 16:47:22 +0300

    CommentBox Testing using Full-Dom Enzyme testing API

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -3,30 +3,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { shallow } from "enzyme";
 
-import App from "../App";
-import CommentBox from "../CommentBox";
-import CommentList from "../CommentList";
+import App from "components/App";
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentList";
+
+let wrapped;
+
+beforeEach(() => {
+    wrapped = shallow(<App />);
+});
 
 it('shows a comment box', ()=> {
-    
-    
-    // using Enzyme library to test react components
-    // documentaion link https://enzymejs.github.io/enzyme/
-    /* 
-        Enzyme API
-            Static
-                render the given component and return plain HTML
-            Shallow
-                render *just* the given component and none of its children
-            Full DOM
-                render the component and al of its children + let us modify it afterwards
-     */
-
-    const wrapped = shallow(<App />);
-    expect(wrapped.find(CommentBox).length).toEqual(1 );
+    expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
 it('shows a comment list', ()=>{
-    const wrapped = shallow(<App/>);
-    expect(wrapped.find(CommentList).length).toEqual(1 );
+    expect(wrapped.find(CommentList).length).toEqual(1);
 });
