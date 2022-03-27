4207ce970bbdba70c667adb17210c0f081b2dd82
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-03-27 11:16:55 +0300

    Solution to CORS Errors using cors middleware

--- Back-End/index.js
+++ Back-End/index.js
@@ -3,18 +3,21 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const router = require('./router');
 
 const app = express()
 const port = process.env.PORT || 3300
 
 // DB Setup
-// 
-//mongoose.connect('mongodb://localhost:auth/auth');
 mongoose.connect('mongodb://localhost:27017/auth');
-// mongoose.connect('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false/auth', {userNewUrlParser: "true"});
+
 // App Setup
 app.use(morgan('combined'));
+
+// CORS allow for all Access-Control-Allow-Origin: *
+app.use(cors())
 app.use(bodyParser.json({ type: '*/*'}));
 router(app);
 
